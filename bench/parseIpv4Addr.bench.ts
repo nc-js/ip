deeb6adfcda64797fd7aecc4c3fbaf2c09650f5a
Samantha Nguyen <contact@samanthanguyen.me>
2024-11-17 14:57:39 -0600

    make benchmarks more accurate

--- bench/parseIpv4Addr.bench.ts
+++ bench/parseIpv4Addr.bench.ts
@@ -11,7 +11,8 @@ const regex1 = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/
 const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)'
 const regex2 = new RegExp(`^${octet}\.${octet}\.${octet}\.${octet}$`)
 
-
+// this uses IPv4Addr.newAddr() because the loose regular expression
+// doesn't validate if each integer is between 0-255
 Deno.bench('parse+create ipv4 address with regex, loose', () => {
 	for(const addr of addresses) {
 		const result = addr.match(regex1) as RegExpMatchArray
@@ -24,15 +25,17 @@ Deno.bench('parse+create ipv4 address with regex, loose', () => {
 	}
 })
 
+// this uses IPv4Addr.tryFromUint8Array() since IPv4Addr.parse()
+// also creates addresses internally with IPv4Addr.tryFromUint8Array()
 Deno.bench('parse+create ipv4 address with regex, strict (checks 0-255)', () => {
 	for(const addr of addresses) {
 		const result = addr.match(regex2) as RegExpMatchArray
-		Ipv4Addr.newAddr(
+		Ipv4Addr.tryFromUint8Array(new Uint8Array([
 			Number.parseInt(result[1], 10),
 			Number.parseInt(result[2], 10),
 			Number.parseInt(result[3], 10),
 			Number.parseInt(result[4], 10)
-		)
+		]))
 	}
 })
 
