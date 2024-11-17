76a417d4ec196cf23c9903b8687509f896947bec
Samantha Nguyen <contact@samanthanguyen.me>
2024-11-17 03:46:20 -0600

    test cases for parsing socket address (ipv4)
    fix minor indexing bugs in parse method impl

--- src/socket.ts
+++ src/socket.ts
@@ -25,21 +25,27 @@ export class SocketAddrV4 {
 	/**
 	 * Parses a string in the format of `"ipv4:port"`,
 	 * where the port is an unsigned 16-bit integer.
+	 *
+	 * This will return undefined if:
+	 * - The IPv4 address cannot be parsed.
+	 * - The IPV4 address can be parsed, but there's no `:` delimiter.
+	 * - The port number can't be parsed, or is out of bounds of an unsigned
+	 *   16-bit integer (0 to 65,535).
 	 */
-	public static fromString(s: string): SocketAddrV4 | undefined {
+	public static parse(s: string): SocketAddrV4 | undefined {
 		const [addr, idx] = parseIpv4Addr(s)
 		if (addr === undefined) {
 			return undefined
 		}
 
-		const afterAddr = idx + 1
+		const afterAddr = idx
 		if (s[afterAddr] !== ':') {
 			return undefined
 		}
 
-		const [portStr, _] = takeAsciiDigits(s, afterAddr)
-		const portNum = Number.parseInt(portStr[0])
-		if (Number.isNaN(portNum)) {
+		const [portStr, _] = takeAsciiDigits(s, afterAddr + 1)
+		const portNum = Number.parseInt(portStr)
+		if (Number.isNaN(portNum) || (portNum > 65535)) {
 			return undefined
 		}
 
---
+++ src/socket_test.ts
@@ -0,0 +1,30 @@
+import { assert, assertEquals } from '@std/assert'
+import { Ipv4Addr } from './ipv4.ts'
+import { SocketAddrV4 } from './socket.ts'
+
+Deno.test('socket addresss v4: parse is ok', () => {
+	const socket = SocketAddrV4.parse('127.0.0.1:8080')
+	assert(socket instanceof SocketAddrV4)
+	assert(socket.addr.equals(Ipv4Addr.LOCALHOST))
+	assertEquals(socket.port, 8080)
+})
+
+Deno.test('socket address v4: parse is error, empty string', () => {
+	const socket = SocketAddrV4.parse('')
+	assertEquals(socket, undefined)
+})
+
+Deno.test('socket address v4: parse is error, no colon delimiter', () => {
+	const socket = SocketAddrV4.parse('127.0.0.1!8080')
+	assertEquals(socket, undefined)
+})
+
+Deno.test('socket address v4: parse is error, no port', () => {
+	const socket = SocketAddrV4.parse('127.0.0.1:')
+	assertEquals(socket, undefined)
+})
+
+Deno.test('socket address v4: parse is error, port is out of bounds', () => {
+	const socket = SocketAddrV4.parse('127.0.0.1:65536')
+	assertEquals(socket, undefined)
+})
