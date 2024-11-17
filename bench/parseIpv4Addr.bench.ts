56439c236a5a6952c92939d2abf1a6cf11ce1bbe
Samantha Nguyen <contact@samanthanguyen.me>
2024-11-17 14:52:11 -0600

    precompile regexes in benchmark

--- bench/parseIpv4Addr.bench.ts
+++ bench/parseIpv4Addr.bench.ts
@@ -6,10 +6,15 @@ for(let i = 0; i < 100_000; i++) {
 	addresses.push(getRandomAddress())
 }
 
-Deno.bench('parse+create ipv4 address with regex, 1', () => {
-	const regex = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/
+// precompile regular expressions
+const regex1 = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/
+const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)'
+const regex2 = new RegExp(`^${octet}\.${octet}\.${octet}\.${octet}$`)
+
+
+Deno.bench('parse+create ipv4 address with regex, loose', () => {
 	for(const addr of addresses) {
-		const result = addr.match(regex) as RegExpMatchArray
+		const result = addr.match(regex1) as RegExpMatchArray
 		Ipv4Addr.newAddr(
 			Number.parseInt(result[1], 10),
 			Number.parseInt(result[2], 10),
@@ -19,13 +24,9 @@ Deno.bench('parse+create ipv4 address with regex, 1', () => {
 	}
 })
 
-// Stricter regex which also checks that integers are between 0-255
-Deno.bench('parse+create ipv4 address with regex, 2', () => {
-	const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)'
-	const regex = new RegExp(`^${octet}\.${octet}\.${octet}\.${octet}$`)
-
+Deno.bench('parse+create ipv4 address with regex, strict (checks 0-255)', () => {
 	for(const addr of addresses) {
-		const result = addr.match(regex) as RegExpMatchArray
+		const result = addr.match(regex2) as RegExpMatchArray
 		Ipv4Addr.newAddr(
 			Number.parseInt(result[1], 10),
 			Number.parseInt(result[2], 10),
