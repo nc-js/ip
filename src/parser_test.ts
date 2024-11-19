448a4f9123b206603aaeb3446ee53f68eea7c080
Samantha Nguyen <contact@samanthanguyen.me>
2024-11-19 00:16:43 -0600

    Update parser_test.ts

--- src/parser_test.ts
+++ src/parser_test.ts
@@ -3,7 +3,10 @@ import { parseIpv4Addr } from './parser.ts'
 import { Ipv4Addr } from './ipv4.ts'
 
 Deno.test('parseIpv4Addr is relaxed, only parses up to valid input', () => {
-	const [ip, index] = parseIpv4Addr('127.0.0.1...')
+	const s = '127.0.0.1...'
+	const [ip, index] = parseIpv4Addr(s)
+
 	assertEquals(ip, Ipv4Addr.newAddr(127, 0, 0, 1))
 	assertEquals(index, 9)
+	assertEquals(s.slice(index), '...')
 })
